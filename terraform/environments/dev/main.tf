a2f20b65b5e4a9c50e00f9ed90de7354d8b9bc21
Abhishek <abhizd1999@gmail.com>
2026-03-19 18:40:01 +0000

    Fix: Use Debian image and correct IP

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -16,7 +16,7 @@ module "networking" {
   environment = "dev"
   region      = var.region
   subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
-  admin_ips   = ["YOUR_OFFICE_IP/32"]
+  admin_ips   = ["176.61.39.68/32"]
 }
 
 module "compute" {
@@ -28,7 +28,7 @@ module "compute" {
   subnet_id      = module.networking.subnet_ids[0]
   machine_type   = "e2-medium"
   instance_count = 2
-  source_image   = "ubuntu-os-cloud/ubuntu-2004-lts"
+  source_image   = "debian-cloud/debian-12"
   disk_size      = 50
   startup_script = ""
   tags           = ["dev", "webserver"]
