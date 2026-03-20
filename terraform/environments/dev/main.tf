08091e62d1ee8d9e6d5caace787e1418690ed443
Abhishek <abhizd1999@gmail.com>
2026-03-20 02:50:01 +0000

    added config file

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -33,3 +33,40 @@ module "compute" {
   startup_script = ""
   tags           = ["dev", "webserver"]
 }
+
+# Enable Monitoring API
+resource "google_project_service" "monitoring" {
+  service = "monitoring.googleapis.com"
+  disable_on_destroy = false
+}
+
+# Email notification channel
+resource "google_monitoring_notification_channel" "dev_email" {
+  display_name = "Dev Environment Email Alerts"
+  type         = "email"
+  labels = {
+    email_address = "abhishekzod@gmail.com"  # Change to your email
+  }
+}
+
+# CPU Alert
+resource "google_monitoring_alert_policy" "cpu_alert" {
+  display_name = "Dev - High CPU Usage"
+  combiner     = "OR"
+  
+  conditions {
+    display_name = "CPU > 80%"
+    condition_threshold {
+      filter     = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.labels.project_id=\"dev-env-project-490714\""
+      duration   = "300s"
+      comparison = "COMPARISON_GT"
+      threshold_value = 0.8
+      aggregations {
+        alignment_period     = "60s"
+        per_series_aligner = "ALIGN_MEAN"
+      }
+    }
+  }
+  
+  notification_channels = [google_monitoring_notification_channel.dev_email.name]
+}
